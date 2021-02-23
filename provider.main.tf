b98b6a767b082696526c02ab6c6efbc09c667487
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-02-22 21:51:58 -0600

    Update provider.main.tf

--- provider.main.tf
+++ provider.main.tf
@@ -1,13 +1,12 @@
-
-## Atlas Provider - Main ##
-
-# Define Terraform provider
 terraform {
-  required_version = ">= 0.12"
+  required_providers {
+    mongodbatlas = {
+      source = "mongodb/mongodbatlas"
+      version = "0.8.2"
+    }
+  }
 }
 
-# Define the MongoDB Atlas Provider
 provider "mongodbatlas" {
-  public_key  = var.atlas_public_key
-  private_key = var.atlas_private_key
+  # Configuration options
 }
