a441b41a4bfb9a8770ffb5fedf7933b7795770fd
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-02-25 16:20:43 -0600

    Update atlas-main.tf

--- atlas-main.tf
+++ atlas-main.tf
@@ -17,7 +17,7 @@ resource "random_password" "db-user-password" {
 
 # Create a Database User
 resource "mongodbatlas_database_user" "db-user" {
-  username = "galaxy-read"
+  username = "slb-test"
   password = random_password.db-user-password.result
   project_id = mongodbatlas_project.atlas-project.id
   auth_database_name = "admin"
